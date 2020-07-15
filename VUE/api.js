0aed3ff34994d16f7bd09b4eb7cec3581eacc631
wlll122 <wll_zj122@outlook.com>
2020-07-15 21:57:31 +0800

    0715

---
+++ VUE/api.js
@@ -0,0 +1,7 @@
+
+
+let move ={
+    hotMove:`http://bufantec.com/api/douban/movie/in_theaters?start=1&limit=10`,
+    coming:`http://bufantec.com/api/douban/movie/coming_soon?start=1&limit=10`,
+    top:`http://bufantec.com/api/douban/movie/top250?start=1&limit=10`
+}
